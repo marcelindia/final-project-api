20d8031d0b94217f5638310616391d025dee466c
Diana Marcelin <marcelindiana@yahoo.com>
2022-03-16 11:17:45 -0400

    created path to update rating

--- index.js
+++ index.js
@@ -3,23 +3,27 @@ const cors = require("cors");
 const { connectDb } = require("./src/ConnectDb");
 const PORT = process.env.PORT || 3000;
 // const { createUser, getUser, updateUser } = require("./src/Users");
+
+// app.post("/users", createUser);
+// app.get("/users", getUser);
+// app.patch("/users/:userId", updateUser);
+
 const {
   getRecipe,
   createRecipe,
   getRecipeByIngredient,
+  getRecipeById,
+  updateRecipeById,
 } = require("./src/Recipes");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-// app.post("/users", createUser);
-// app.get("/users", getUser);
-// app.patch("/users/:userId", updateUser);
-
-app.get("/recipes/:ingredient", getRecipeByIngredient);
+app.get("/recipes/recipe/:ingredient", getRecipeByIngredient);
+app.get("/recipes/:id", getRecipeById);
 app.get("/recipes", getRecipe);
-app.post("/recipes", createRecipe);
+app.patch("/recipes/:id", updateRecipeById);
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
--- src/Recipes.js
+++ src/Recipes.js
@@ -1,17 +1,28 @@
 const { connectDb } = require("./ConnectDb");
 const { response } = require("express");
 
-exports.createRecipe = (req, res) => {
-  const newRecipe = req.body;
+exports.createRecipeComment = (req, res) => {
+  const newRecipeComment = req.body;
   const db = connectDb();
   db.collection("recipes")
     .add(newRecipe)
     .then((doc) => res.status(201).send(doc.id))
     .catch((err) => res.status(500).send(err));
 };
+
 exports.getRecipe = (req, res) => {
   const db = connectDb();
-  db.collection("recipes")
+  const { ing } = req.query;
+  let ingArr = [];
+
+  let query = db.collection("recipes");
+
+  if (ing) {
+    ingArr = ing.split(",");
+    query = query.where("ingredients", "array-contains-any", ingArr);
+  }
+
+  query
     .get()
     .then((snapshot) => {
       const recipeList = snapshot.docs.map((doc) => {
@@ -28,7 +39,7 @@ exports.getRecipeByIngredient = (req, res) => {
   const db = connectDb();
   const { ingredient } = req.params;
   db.collection("recipes")
-    .where("ingredients", "array-contains-any", [ingredient])
+    .where("ingredients", "array-contains-any", [recipes.ingredient])
     .get()
     .then((snapshot) => {
       const recipeList = snapshot.docs.map((doc) => {
@@ -38,5 +49,30 @@ exports.getRecipeByIngredient = (req, res) => {
       });
       res.send(recipeList);
     })
+    .catch((err) => res.status(500).send(err));
+};
+exports.getRecipeById = (req, res) => {
+  const db = connectDb();
+  const { id } = req.params;
+  db.collection("recipes")
+    .doc(id)
+    .get()
+    .then((doc) => {
+      let recipe = doc.data();
+      recipe.id = doc.id;
+      res.send(recipe);
+    })
+    .catch((err) => res.status(500).send(err));
+};
+
+exports.updateRecipeById = (req, res) => {
+  const db = connectDb();
+  const { id } = req.params;
+  // recipe.id = doc.id;
+  const updateRating = req.body;
+  db.collection("recipes")
+    .doc(id)
+    .update(updateRating)
+    .then((doc) => res.status(202).send(doc))
     .catch((err) => res.status(500).send(err));
 };
